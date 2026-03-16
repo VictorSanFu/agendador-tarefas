27f6612dad068a306883a522908c7c64e5e85947
Victor <vsantiagofu@gmail.com>
2026-03-16 10:54:04 -0300

    commit inicial com esqueleto

---
+++ agendador-tarefas/src/test/java/com/javanauta/agendadortarefa/AgendadorTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendadortarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
